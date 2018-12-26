0e674be8c88b1fca5454f703ad4e7654ab078dcc
Jiwon Hwang <hjw5167@gmail.com>
2018-12-26 14:46:24 +0900

    srcnn updated

---
+++ model.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+from blocks import ConvBlock
+
+
+class SRCNN(nn.Module):
+    def __init__(self):
+        super(SRCNN, self).__init__()
+
+        self.layers = nn.Sequential(
+            ConvBlock(3, 256,  kernel_size=9, padding=4),
+            ConvBlock(256, 128, kernel_size=1, padding=0),
+            ConvBlock(128, 3,  kernel_size=5, padding=2, activation=None))
+    
+    def forward(self, x):
+        return self.layers(x)
